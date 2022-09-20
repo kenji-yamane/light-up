ac01b2225ad1d5549ccbae769a158a6b94315585
Monk <kenjiyamane0@gmail.com>
2022-09-20 13:46:15 -0300

    reformat testmain

--- tst/main.cpp
+++ tst/main.cpp
@@ -20,50 +20,62 @@ bool testCase(const std::string& game, const std::string& expectedSolution) {
 
 int main() {
     std::vector<std::string> games, expectedSolutions;
-    games.push_back(std::string("- - o o o o 1\n")+
-                    std::string("o o o o o o 1\n")+
-                    std::string("o o o o o o o\n")+
-                    std::string("o o o 0 o o o\n")+
-                    std::string("o o o o o o o\n")+
-                    std::string("2 o o o o o o\n")+
-                    std::string("- o o o o 1 -\n"));
-    expectedSolutions.push_back(std::string("|-|-| | | |L|1|\n")+
-                                std::string("| | |L| | | |1|\n")+
-                                std::string("| | | | | | |L|\n")+
-                                std::string("| | | |0| | | |\n")+
-                                std::string("|L| | | | | | |\n")+
-                                std::string("|2|L| | | | | |\n")+
-                                std::string("|-| | | |L|1|-|\n"));
+    games.push_back(
+        std::string("- - ? ? ? ? 1\n")+
+        std::string("? ? ? ? ? ? 1\n")+
+        std::string("? ? ? ? ? ? o\n")+
+        std::string("? ? ? 0 ? ? o\n")+
+        std::string("? ? ? ? ? ? o\n")+
+        std::string("2 ? ? ? ? ? o\n")+
+        std::string("- ? ? ? ? 1 -\n")
+    );
+    expectedSolutions.push_back(
+        std::string("|-|-| | | |L|1|\n")+
+        std::string("| | |L| | | |1|\n")+
+        std::string("| | | | | | |L|\n")+
+        std::string("| | | |0| | | |\n")+
+        std::string("|L| | | | | | |\n")+
+        std::string("|2|L| | | | | |\n")+
+        std::string("|-| | | |L|1|-|\n")
+    );
 
-    games.push_back(std::string("o o o o - o o\n")+
-                    std::string("o o o 3 o o o\n")+
-                    std::string("0 o o o o o o\n")+
-                    std::string("o 1 o o o - o\n")+
-                    std::string("o o o o o o 3\n")+
-                    std::string("o o o - o o o\n")+
-                    std::string("o o 2 o o o o\n"));
-    expectedSolutions.push_back(std::string("| | | |L|-|L| |\n")+
-                                std::string("| |L| |3|L| | |\n")+
-                                std::string("|0| | |L| | | |\n")+
-                                std::string("| |1|L| | |-|L|\n")+
-                                std::string("| | | | | |L|3|\n")+
-                                std::string("|L| | |-| | |L|\n")+
-                                std::string("| |L|2|L| | | |\n"));
+    games.push_back(
+        std::string("? ? ? ? - ? o\n")+
+        std::string("? ? ? 3 ? ? o\n")+
+        std::string("0 ? ? ? ? ? o\n")+
+        std::string("? 1 ? ? ? - o\n")+
+        std::string("? ? ? ? ? ? 3\n")+
+        std::string("? ? ? - ? ? o\n")+
+        std::string("? ? 2 ? ? ? o\n")
+    );
+    expectedSolutions.push_back(
+        std::string("| | | |L|-|L| |\n")+
+        std::string("| |L| |3|L| | |\n")+
+        std::string("|0| | |L| | | |\n")+
+        std::string("| |1|L| | |-|L|\n")+
+        std::string("| | | | | |L|3|\n")+
+        std::string("|L| | |-| | |L|\n")+
+        std::string("| |L|2|L| | | |\n")
+    );
 
-    games.push_back(std::string("0 o - 0 o o 0\n")+
-                    std::string("o o o o o o o\n")+
-                    std::string("o o o o o o -\n")+
-                    std::string("0 o o o o o 2\n")+
-                    std::string("- o o o o o o\n")+
-                    std::string("o o o o o o o\n")+
-                    std::string("1 o o 0 0 o -\n"));
-    expectedSolutions.push_back(std::string("|0| |-|0| | |0|\n")+
-                                std::string("| | | | |L| | |\n")+
-                                std::string("| | | |L| | |-|\n")+
-                                std::string("|0| | | | |L|2|\n")+
-                                std::string("|-| | | | | |L|\n")+
-                                std::string("| | |L| | | | |\n")+
-                                std::string("|1|L| |0|0| |-|\n"));
+    games.push_back(
+        std::string("0 ? - 0 ? ? 0\n")+
+        std::string("? ? ? ? ? ? o\n")+
+        std::string("? ? ? ? ? ? -\n")+
+        std::string("0 ? ? ? ? ? 2\n")+
+        std::string("- ? ? ? ? ? o\n")+
+        std::string("? ? ? ? ? ? o\n")+
+        std::string("1 ? ? 0 0 ? -\n")
+    );
+    expectedSolutions.push_back(
+        std::string("|0| |-|0| | |0|\n")+
+        std::string("| | | | |L| | |\n")+
+        std::string("| | | |L| | |-|\n")+
+        std::string("|0| | | | |L|2|\n")+
+        std::string("|-| | | | | |L|\n")+
+        std::string("| | |L| | | | |\n")+
+        std::string("|1|L| |0|0| |-|\n")
+    );
 
     for (int i = 0; i < (int)games.size(); i++) {
         if (not testCase(games[i], expectedSolutions[i])) {
